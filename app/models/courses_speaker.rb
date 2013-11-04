38b53b2e735b8a1081af7fcf4f3a01b12c17122c
Dane Jorgensen <dane@bgbng.com>
2013-11-04 16:53:21 -0700

    updated seeds

--- app/models/courses_speaker.rb
+++ app/models/courses_speaker.rb
@@ -1,4 +1,6 @@
 class CoursesSpeaker < ActiveRecord::Base
+  attr_accessible :course_id, :speaker_id
+
   belongs_to :course
   belongs_to :speaker
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
 Admin::Course.create!(name: 'Course 1')
 Admin::Course.create!(name: 'Course 2')
+
+Admin::Event.create!(name: 'Event 1')
+Admin::Event.create!(name: 'Event 2')
+
+Admin::Speaker.create!(first_name: 'John', last_name: 'Doe')
+Admin::Speaker.create!(first_name: 'Jane', last_name: 'Doe')
+
+CoursesSpeaker.create!(course_id: 1, speaker_id: 1)
+EventsSpeaker.create!(event_id: 1, speaker_id: 1)
+CoursesSpeaker.create!(course_id: 2, speaker_id: 2)
+EventsSpeaker.create!(event_id: 2, speaker_id: 2)
